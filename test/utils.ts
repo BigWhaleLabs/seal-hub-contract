0df56e7b2591dee9fcb2cc6a7e82dd2b9909fba1
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-11-03 22:39:17 +0400

    remove unnecessary linebreaks

--- test/utils.ts
+++ test/utils.ts
@@ -9,16 +9,13 @@ export const zeroAddress = '0x0000000000000000000000000000000000000000'
 export async function getIncrementalTreeContract() {
   const poseidonT3ABI = poseidonContract.generateABI(2)
   const poseidonT3Bytecode = poseidonContract.createCode(2)
-
   const [signer] = await ethers.getSigners()
-
   const PoseidonLibT3Factory = new ethers.ContractFactory(
     poseidonT3ABI,
     poseidonT3Bytecode,
     signer
   )
   const poseidonT3Lib = await PoseidonLibT3Factory.deploy()
-
   await poseidonT3Lib.deployed()
 
   const IncrementalBinaryTreeLibFactory = await ethers.getContractFactory(
@@ -31,7 +28,6 @@ export async function getIncrementalTreeContract() {
   )
   const incrementalBinaryTreeLib =
     await IncrementalBinaryTreeLibFactory.deploy()
-
   await incrementalBinaryTreeLib.deployed()
 
   return incrementalBinaryTreeLib.address
